45f20d5d0e9de34ae43523dec95b2bb018ac85fa
SNAPP1X <101673454+SNAPP1X@users.noreply.github.com>
2023-04-02 21:22:37 +0300

    Add files via upload

---
+++ TicketsProject/DB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsProject
+{
+    internal class DB
+    {
+        SqlConnection connection = new SqlConnection("Data Source=WIN-E818I8RJBE2\\SNAPPIXPC;Initial Catalog=Бронирование_билетов;Integrated Security=True");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        public SqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
